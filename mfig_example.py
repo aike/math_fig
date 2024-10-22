36fd656426d26a9a9da5ba4d659d95144a07c423
aike <aaiikkee@gmail.com>
2024-10-22 14:00:02 +0900

    add example

--- mfig_example.py
+++ mfig_example.py
@@ -1,6 +1,6 @@
 import mfig
 
-mfig.init()
+mfig.init(max=7)
 
 mfig.vec(3, 1,'red')
 mfig.vec(5, 6,'blue')
@@ -10,8 +10,8 @@ mfig.vec2(3+0.2, 1+0.2, 5+0.2, 6-0.2,'#aaaaaa')
 mfig.dot(3, 1)
 mfig.dot(5, 6)
 
-mfig.text(3.5, 1, "P(3,1)", 'xx-large')
-mfig.text(5.5, 5.5, "P'(5,6)", 'xx-large')
+mfig.text(3.5, 1, "P(3,1)")
+mfig.text(5.5, 5.5, "P'(5,6)")
 
 mfig.show()
 
